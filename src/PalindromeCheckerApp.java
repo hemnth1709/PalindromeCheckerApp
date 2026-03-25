cc2f18ceb3481a8f24836ac83c6701d98c375cfd
new-cde <abinaya.muthukrishnan.3006@gmail.com>
2026-03-25 15:12:44 +0530

    UC2: Check hardcoded string palindrome using if-else

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,14 +2,22 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        // Welcome Message
         System.out.println("=== Palindrome Checker App ===");
 
-        // Application Information
-        System.out.println("Application Name : Palindrome Checker");
-        System.out.println("Version : 1.0");
+        // Hardcoded string
+        String word = "madam";
 
-        System.out.println("Application started successfully.");
-        System.out.println("Ready to check palindromes...");
+        // Reverse the string
+        String reversed = "";
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        // Check palindrome
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a Palindrome.");
+        } else {
+            System.out.println(word + " is NOT a Palindrome.");
+        }
     }
 }
