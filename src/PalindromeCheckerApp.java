7c0e3b328f16e984f78b2f6740c9744c7e2beb10
new-cde <abinaya.muthukrishnan.3006@gmail.com>
2026-03-25 15:15:09 +0530

    UC3: Reverse string using loop and compare to check palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -4,16 +4,18 @@ public class PalindromeCheckerApp {
 
         System.out.println("=== Palindrome Checker App ===");
 
-        // Hardcoded string
-        String word = "madam";
+        // Original string
+        String word = "level";
 
-        // Reverse the string
+        // Variable to store reversed string
         String reversed = "";
+
+        // Reverse the string using for loop
         for (int i = word.length() - 1; i >= 0; i--) {
             reversed = reversed + word.charAt(i);
         }
 
-        // Check palindrome
+        // Compare original and reversed strings
         if (word.equals(reversed)) {
             System.out.println(word + " is a Palindrome.");
         } else {
