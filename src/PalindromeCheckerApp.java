d726e97db0c056b7fd0a94c407f0bcd7bd75b4da
new-cde <abinaya.muthukrishnan.3006@gmail.com>
2026-03-25 15:09:05 +0530

    UC1: Display welcome message and application information on startup

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,15 @@
 public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Welcome Message
+        System.out.println("=== Palindrome Checker App ===");
+
+        // Application Information
+        System.out.println("Application Name : Palindrome Checker");
+        System.out.println("Version : 1.0");
+
+        System.out.println("Application started successfully.");
+        System.out.println("Ready to check palindromes...");
+    }
 }
